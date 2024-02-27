65f0498d702b8c58410c5f8aa3fa278011234cd0
codesver <codesver@gmail.com>
2024-02-27 13:37:57 +0900

    fix: remove authentication router and just check authorization

--- src/router/AuthRouter.tsx
+++ src/router/AuthRouter.tsx
@@ -4,7 +4,7 @@ import Auth, { Role } from "../core/Auth";
 const AuthRouter = () => {
   return (
     <Routes>
-      <Route path="/admin" element={Auth.authorized(Role.ADMIN) ? <></> : <Navigate to={"/login"} replace />} />
+      <Route path="/admin" element={Auth.authorized(Role.ADMIN) ? <></> : <Navigate to={"/login"} />} />
     </Routes>
   );
 };
